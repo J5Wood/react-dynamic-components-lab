33d75ab202ec49d21fd1758aac9b9a437fb2c148
Jeremy Wood <js.wood206@gmail.com>
2021-04-23 22:55:33 -0700

    Removed unnecessary code

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
 
-  state = {
-    todos: [
-      
-    ]
-  }
-
   render() {
     return this.props.opacity < .2 ? null : (
       <div className="color-box" style={{opacity: this.props.opacity}}>
